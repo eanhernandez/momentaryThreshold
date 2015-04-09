c24a93e738a5e5916251f61af41316d176bc0d34
eanhernandez <ean.hernandez@eanh.net>
2015-04-09 11:36:41 -0700

    added args

--- momentaryThreshold.c
+++ momentaryThreshold.c
@@ -1,39 +1,43 @@
 #include <wiringPi.h>
 #include <stdio.h>
-int main (void)
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
 {
   wiringPiSetup () ;
 
   //setting up pins
-  pinMode (4, INPUT) ;
   pinMode (0, OUTPUT) ;
   pinMode (2, OUTPUT) ; 
   digitalWrite (2, HIGH);
-  int state=0;
-  for (;;)
-  {
-	if (digitalRead(4)!=state)
-	{
-		if (state==0)
-		{
-			digitalWrite (0, HIGH) ; delay (250) ;
-			digitalWrite (0,  LOW) ; 
-	                //printf("latch on --> flip momentary \n");
-        	        //fflush(stdout);
-			state=1;
-		}
-		else
-		{
-                        digitalWrite (0, HIGH) ; delay (250) ;
-                        digitalWrite (0,  LOW) ;
 
+  int i=0;
+  int max=250;
+  int iDelay=100;
 
-                        //printf("latch off --> flip momentary \n");
-                        //fflush(stdout);
-			state=0;
-		}
-	}	
-  	delay(1);
+  if (argc==3)
+  { 
+	max = atoi(argv[1]);
+	iDelay = atoi(argv[2]);
   }
-  return 0 ;
+  else
+  {
+        printf("requires two arguments: max and delay.\n");
+        fflush(stdout);
+	return 1;
+  }
+
+  for (i=max;i>=0;i=i-10)
+  {
+  	digitalWrite (0, HIGH) ; 
+	delay (i) ;
+        digitalWrite (0,  LOW) ;
+        printf("test:%i ms.\n",i);
+        fflush(stdout);
+ 	delay(iDelay);
+  }	
+  
+  digitalWrite (2, LOW);
+ 
+  return 0;
 }
